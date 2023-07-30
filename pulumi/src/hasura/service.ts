ada805b7679e2450b4952bf0bed803a70fa5e73d
Lionello Lunesu <lio+git@lunesu.com>
2023-07-30 15:22:59 -0700

    add heimdallJwksEndpoint config

--- pulumi/src/hasura/service.ts
+++ pulumi/src/hasura/service.ts
@@ -1,15 +1,13 @@
 import { DefangService } from '@defang-io/pulumi-defang/lib';
 import * as pulumi from '@pulumi/pulumi';
 import { service as apiService } from '../api/service';
-import { service as heimdallService } from '../heimdall/service';
 import { config, dockerHubToken } from '../common/config';
 import { SERVICE_NAME } from './constants';
 import { hasuraDatabaseUri } from './database';
 import { image } from './image';
-import { ROOT_URL } from '../common/constants';
 
 const authenticatedImageName = pulumi.interpolate`defangportal:${dockerHubToken}@${image.repoDigest}`;
-const heimdallJwksEndpoint = "lionello-heimdall-production--4457.prod1.defang.dev";// TODO: use pulumi.runtime.isDryRun() ? '' : heimdallService.endpoints[1];
+const heimdallJwksEndpoint = config.require("heimdallJwksEndpoint"); // TODO: should be heimdallService.endpoints[1] but circular dependency
 
 export const service: DefangService = new DefangService(SERVICE_NAME, {
     name: `${SERVICE_NAME}-${pulumi.getStack()}`,
@@ -25,7 +23,7 @@ export const service: DefangService = new DefangService(SERVICE_NAME, {
         HASURA_GRAPHQL_ENABLE_REMOTE_SCHEMA_PERMISSIONS: 'true',
         DEFANG_FN_ENDPOINT: pulumi.interpolate`https://${apiService.endpoints[0]}`,
         HASURA_GRAPHQL_EXPERIMENTAL_FEATURES: 'naming_convention',
-        HASURA_GRAPHQL_JWT_SECRET: pulumi.interpolate`{"jwk_url":"https://${heimdallJwksEndpoint}/.well-known/jwks"}`,
+        HASURA_GRAPHQL_JWT_SECRET: pulumi.interpolate`{"jwk_url":"${heimdallJwksEndpoint}"}`, // TODO: use pulumi.jsonStringify
     },
     secrets: [{source: "HASURA_GRAPHQL_ADMIN_SECRET", value: config.requireSecret("hasuraAdminSecret")}],
     platform: 'linux/arm64',
