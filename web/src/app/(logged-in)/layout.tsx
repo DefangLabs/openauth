713f25408733f8492c6f5e21dcd9850d94db0039
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2024-04-25 15:44:57 -0700

    add suspense boundaries

--- web/src/app/(logged-in)/layout.tsx
+++ web/src/app/(logged-in)/layout.tsx
@@ -1,16 +1,13 @@
 "use client";
 
+import { Loader } from "@/components/loader/loader";
 import { analytics } from "@/modules/analytics/lib/analytics";
 import { useDefangClient } from "@/modules/defang/hooks/use-defang-client/use-defang-client";
 import { Placeholder } from "@/modules/kratos/components/login-required/components/placeholder/placeholder";
 import { useSession } from "@/modules/kratos/hooks/use-session/use-session";
 import { useEffect } from "react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+function RootLayoutInner({ children }: { children: React.ReactNode }) {
   const { session, loading } = useSession();
   const client = useDefangClient();
   const display = !!session && !loading;
@@ -27,4 +24,16 @@ export default function RootLayout({
   }, [client, display]);
 
   return display ? <>{children}</> : <Placeholder />;
+}
+
+export default function LoggedInLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Loader>
+      <RootLayoutInner>{children}</RootLayoutInner>
+    </Loader>
+  );
 }
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Providers } from "@/lib/providers/providers";
 import { Roboto } from "next/font/google";
 import { Suspense } from "react";
 import { Layout } from "../components/layout/layout";
+import { Loader } from "@/components/loader/loader";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -10,20 +11,26 @@ export const metadata = {
   description: "Welcome to the Defang Opinionated Platform",
 };
 
+function RootLayoutInner({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>
+        <Providers>
+          <Layout>{children}</Layout>
+        </Providers>
+      </body>
+    </html>
+  );
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>
-        <Providers>
-          <Layout>
-            <Suspense fallback={null}>{children}</Suspense>
-          </Layout>
-        </Providers>
-      </body>
-    </html>
+    <Loader>
+      <RootLayoutInner>{children}</RootLayoutInner>
+    </Loader>
   );
 }
