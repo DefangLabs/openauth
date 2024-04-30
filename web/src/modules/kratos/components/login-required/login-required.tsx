a95a93d3a94fa9153b12383a549914de3957f6bb
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2024-04-30 07:38:22 -0700

    add build test in PRs and allow login required HoC to take props "any"

--- web/src/modules/kratos/components/login-required/login-required.tsx
+++ web/src/modules/kratos/components/login-required/login-required.tsx
@@ -1,10 +1,8 @@
 import { FC } from "react";
 import { Inner } from "./components/inner/inner";
 
-export function LoginRequired<T extends React.Component>(
-  Component: React.ComponentType<T>
-): FC<T> {
-  const Wrapped = (props: T) => <Inner Component={Component} {...props} />;
+export function LoginRequired(Component: React.ComponentType<any>): FC<any> {
+  const Wrapped = (props: any) => <Inner Component={Component} {...props} />;
   Wrapped.displayName = `LoginRequired(${Component.displayName})`;
   return Wrapped;
 }
