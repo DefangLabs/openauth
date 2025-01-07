ba8e79cf8a183861e16cd3b52197fea2ed8f0516
Lionello Lunesu <lio+git@lunesu.com>
2025-01-07 09:47:00 -0800

    Fix clickjacking

--- web/next.config.js
+++ web/next.config.js
@@ -25,10 +25,10 @@ const nextConfig = {
                 key: "Strict-Transport-Security",
                 value: "max-age=31536000; includeSubDomains; preload",
               },
-              // {
-              //   key: "X-Frame-Options",
-              //   value: "DENY",
-              // },
+              {
+                key: "X-Frame-Options",
+                value: "DENY",
+              },
               // {
               //   key: "X-Content-Type-Options",
               //   value: "nosniff",
