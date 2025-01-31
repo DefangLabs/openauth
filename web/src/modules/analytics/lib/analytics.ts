ede5cdbf778c1f1f695eafb76cd5344ca3443684
Jordan Stephens <jordan@stephens.io>
2025-01-31 09:16:51 -0800

    disable segment easily in dev (#140)
    I can't live like this 😅 
    <img width="671" alt="Screenshot 2025-01-30 at 5 18 35 PM"
    src="https://github.com/user-attachments/assets/fe4b045d-5565-4abd-b40a-9ca9036628c0"
    />

--- web/src/modules/analytics/lib/analytics.ts
+++ web/src/modules/analytics/lib/analytics.ts
@@ -1,5 +1,15 @@
 import { AnalyticsBrowser } from "@segment/analytics-next";
 
-export const analytics = AnalyticsBrowser.load({
-  writeKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY!,
-});
+const writeKey = process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY;
+
+export const analytics = AnalyticsBrowser.load(
+  {
+    writeKey: writeKey ?? "",
+  },
+  {
+    // TODO: I can trigger the false branch by removing
+    // NEXT_PUBLIC_SEGMENT_WRITE_KEY from .env, but it should be easier than
+    // that.
+    disable: writeKey ? false : true,
+  },
+);
